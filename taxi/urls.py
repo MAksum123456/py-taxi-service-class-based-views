1876e8c5d3c1083a8dc35917ba79e85b85759488
Max <smolinskijmaksim1@gmail.com>
2025-05-04 11:08:17 +0100

    Fix flake8

--- taxi/urls.py
+++ taxi/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path(
         "manufacturers/",
         ManufacturerListView.as_view(),
-         name="manufacturer-list"
+        name="manufacturer-list"
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
@@ -22,7 +22,7 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/",
         DriverDetailView.as_view(),
-         name="driver-detail"
+        name="driver-detail"
     ),
 ]
 
